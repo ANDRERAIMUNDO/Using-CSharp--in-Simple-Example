e4f3601345abc3617b27da76964cd6963c03dd5b
ANDRERAIMUNDO <9000andre@gmail.com>
2020-07-22 19:32:35 -0300

    Adicionando try e catch no Progam.cs

--- SimpleAccountBank/CSharpExampleAndTest/Program.cs
+++ SimpleAccountBank/CSharpExampleAndTest/Program.cs
@@ -11,39 +11,50 @@ namespace CSharpExampleAndTest
         {
             //entrando com os dados de criacao de uma nova conta
             Console.WriteLine("Create the account");
-            Console.Write("Type of the Account: ");
-            Enum_TypeAccount typeAccount = Enum.Parse<Enum_TypeAccount>(Console.ReadLine());
-            Console.Write("Name of the Bank: ");
-            string nameBank = Console.ReadLine();
-            Console.Write("Agency: ");
-            int agency = int.Parse(Console.ReadLine());
-            Console.Write("Account: ");
-            int account = int.Parse(Console.ReadLine());
-            Console.Write("Digit: ");
-            int digit = int.Parse(Console.ReadLine());
-            DateTime dateTime = DateTime.Now;
-            Console.Write("Id client: ");
-            int id = int.Parse(Console.ReadLine());
-            Console.Write("CPF: ");
-            int cpf = int.Parse(Console.ReadLine());
-            Console.Write("Name of the client: ");
-            string name = Console.ReadLine();
-            Console.Write("brithDate('yyyy/MM/day'): ");
-            DateTime birthDate = DateTime.Parse(Console.ReadLine());
-            //instaciando em memoria
-             Banc banc = new Banc (typeAccount, nameBank, agency, account, digit, dateTime, id, cpf, name, birthDate);
-            // exibir resultado
-            Console.WriteLine();
-            Console.WriteLine("Create Account");
-            Console.WriteLine("Bank " + banc.Name);
-            Console.WriteLine("Ageny: " + banc.Agency);
-            Console.WriteLine(" Account: " + banc.Account + " - " + banc.Digit.ToString());
-            Console.WriteLine("Type Acconut: " + typeAccount);
-            Console.WriteLine("ID: " + banc.Users.Id.ToString());
-            Console.WriteLine("CPF: " + banc.Users.Cpf.ToString());
-            Console.WriteLine("Client: " + banc.Users.Name);
-            Console.WriteLine("Brith Date: " + banc.Users.BirthDate);
-            Console.WriteLine("Date this operation: " + banc.Users.DateTime);
+
+            try
+            {
+
+                Console.Write("Type of the Account: ");
+                Enum_TypeAccount typeAccount = Enum.Parse<Enum_TypeAccount>(Console.ReadLine());
+                Console.Write("Name of the Bank: ");
+                string nameBank = Console.ReadLine();
+                Console.Write("Agency: ");
+                int agency = int.Parse(Console.ReadLine());
+                Console.Write("Account: ");
+                int account = int.Parse(Console.ReadLine());
+                Console.Write("Digit: ");
+                int digit = int.Parse(Console.ReadLine());
+                DateTime dateTime = DateTime.Now;
+                Console.Write("Id client: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("CPF: ");
+                int cpf = int.Parse(Console.ReadLine());
+                Console.Write("Name of the client: ");
+                string name = Console.ReadLine();
+                Console.Write("brithDate('yyyy/MM/day'): ");
+                DateTime birthDate = DateTime.Parse(Console.ReadLine());
+                //instaciando em memoria
+                Banc banc = new Banc(typeAccount, nameBank, agency, account, digit, dateTime, id, cpf, name, birthDate);
+                // exibir resultado
+                Console.WriteLine();
+                Console.WriteLine("Create Account");
+                Console.WriteLine("Bank " + banc.Name);
+                Console.WriteLine("Ageny: " + banc.Agency);
+                Console.WriteLine(" Account: " + banc.Account + " - " + banc.Digit.ToString());
+                Console.WriteLine("Type Acconut: " + typeAccount);
+                Console.WriteLine("ID: " + banc.Users.Id.ToString());
+                Console.WriteLine("CPF: " + banc.Users.Cpf.ToString());
+                Console.WriteLine("Client: " + banc.Users.Name);
+                Console.WriteLine("Brith Date: " + banc.Users.BirthDate);
+                Console.WriteLine("Date this operation: " + banc.Users.DateTime);
+            }
+
+            catch (FormatException e)
+            {
+                Console.WriteLine("error has occurred"  + e.Message);
+                return;
+            }
             Console.ReadLine();
         }
     }
